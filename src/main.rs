928f70f31897bf44a68aa11648f8dbf68172a951
Evan Cameron <cameron.evan@gmail.com>
2016-08-16 17:34:05 -0400

    making headway

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use std::thread::Thread;
+use std::sync::{Arc, RwLock};
+
+
+pub type Children<T> = Vec<NodeRef<T>>;
+pub struct NodeRef<T>(Arc<RwLock<Node<T>>>);
+// pub struct Children<T>(Arc<RwLock<Vec<T>>>);
+//
+// impl<T> Children<T> {
+//     fn new() -> Children<T> {
+//         Children(Arc::new(RwLock::new(Vec::new())))
+//     }
+// }
+
+pub struct Node<T> {
+    parent: Option<NodeRef<T>>,
+    children: Arc<RwLock<Children<T>>>,
+    value: T,
+}
+
+impl<T> Node<T> {
+    pub fn new(parent_: Option<NodeRef<T>>, value_: T) -> Node<T> {
+        Node::<T> {
+            parent: parent_,
+            children: Arc::new(RwLock::new(Vec::new())),
+            value: value_,
+        }
+    }
+    pub fn add_child(&self, child: Node<T>) {
+        self.children
+            .write()
+            .expect("Failed to get write lock on node")
+            .push(child);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let parent = Node::new(Option::None, 1u8);
+    let child = Node::new(parent.parent, 2u8);
+    parent.add_child(child);
 }
