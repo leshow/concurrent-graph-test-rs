fcec4714c8615f65d1c4c9f9cc6ebbd528c7059b
Evan Cameron <cameron.evan@gmail.com>
2016-08-16 18:01:26 -0400

    has parents with explicit set parent

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::sync::{Arc, RwLock};
 type NodeRef<T> = Arc<RwLock<_Node<T>>>;
 
 struct _Node<T> {
-    // parent: Option<NodeRef<T>>,
+    parent: Option<NodeRef<T>>,
     children: Vec<NodeRef<T>>,
     value: T,
 }
@@ -13,25 +13,35 @@ struct _Node<T> {
 pub struct Node<T>(NodeRef<T>);
 
 impl<T> Node<T> {
-    pub fn new(parent_: Option<Node<T>>, value_: T) -> Node<T> {
+    pub fn new(value_: T) -> Node<T> {
         let node = _Node {
-            // parent: parent_,
+            parent: Option::None,
             children: Vec::new(),
             value: value_,
         };
         Node(Arc::new(RwLock::new(node)))
     }
-    pub fn add_child(&self, child: Node<T>) {
+    pub fn add_child(&self, child: &Node<T>) {
         self.0
             .write()
             .expect("Failed to get write lock on node")
             .children
             .push(child.0.clone())
     }
+    pub fn set_parent(&self, parent: &Node<T>) {
+        self.0
+            .write()
+            .expect("Failed to get write lock on node")
+            .parent = Option::Some(parent.0.clone());
+
+    }
 }
 
 fn main() {
-    let parent = Node::new(Option::None, 1u8);
-    // let child = Node::new(Option::Some(parent), 2u8);
-    // parent.add_child(NodeRef(child.clone()));
+    let parent = Node::new(1u8);
+    let child = Node::new(2u8);
+    child.set_parent(&parent);
+    parent.add_child(&child);
+
+
 }
